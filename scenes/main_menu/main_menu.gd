39e5332c64eed61488d50b95f7460315cb3b415f
Tristan Greaves <tristan@extricate.org>
2024-04-02 17:19:09 +0100

    Main menu work.

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -4,8 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Logging.log_entry("Entered main menu.")
-
+	
+	$CanvasLayer/VersionLabel.text = "Version " + Constants.GAME_VERSION
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_exit_game_pressed():
+	get_tree().quit()
